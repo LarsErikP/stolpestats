25ba7b23fde1bb0a728164bf3795f4c320033837
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-06-24 00:25:53 +0200

    Cleanup

--- getStolpeStats.py
+++ getStolpeStats.py
@@ -3,8 +3,8 @@ import csv
 from datetime import datetime
 from dotenv import load_dotenv
 from stravalib import Client
-import requests
 
+# Functions
 def m_to_km(dist):
     return round(dist/1000,2)
 
@@ -14,8 +14,7 @@ def mps_to_kmt(speed):
 def closest_mountain(target_height):
     return min(fjell.items(), key=lambda item: abs(item[1] - target_height))
 
-load_dotenv()
-
+# Global variables
 stolpeturer = { "count": 0, 
                "total_distance": 0.0,
                "total_elevation_gain": 0.0,
@@ -25,6 +24,9 @@ stolpeturer = { "count": 0,
 
 after = "2025-01-01"
 fjell = {}
+
+# Load some data from files
+load_dotenv()
 
 with open('fjell.txt', newline='') as fjellfil:
     fjell_data = csv.reader(fjellfil, delimiter=';')
@@ -36,14 +38,14 @@ with open('fjell.txt', newline='') as fjellfil:
 with open('tokens.json', "r") as f:
     token_refresh = json.load(f)
 
+# Auth
 client = Client(
         access_token=token_refresh["access_token"],
         refresh_token=token_refresh["refresh_token"],
         token_expires=token_refresh["expires_at"],
  )
 
-
-
+# Logic starts here
 activities = client.get_activities(after=after)
 for activity in activities:
     if "stolpe" in activity.name.casefold():
@@ -53,14 +55,16 @@ for activity in activities:
         if activity.distance > stolpeturer["longest_trip"]["distance"]:
             stolpeturer["longest_trip"]["distance"] = activity.distance
             stolpeturer["longest_trip"]["name"] = activity.name
-            stolpeturer["longest_trip"]["date"]= activity.start_date.strftime("%d.%m.%Y")
+            stolpeturer["longest_trip"]["date"] = activity.start_date.strftime("%d.%m.%Y")
 
         if activity.average_speed > stolpeturer["fastest_trip"]["avg_speed"]:
             stolpeturer["fastest_trip"]["avg_speed"] = activity.average_speed
             stolpeturer["fastest_trip"]["name"] = activity.name
-            stolpeturer["fastest_trip"]["date"]= activity.start_date.strftime("%d.%m.%Y")
+            stolpeturer["fastest_trip"]["date"] = activity.start_date.strftime("%d.%m.%Y")
 
 mountain = closest_mountain(stolpeturer["total_elevation_gain"])
+
+# Printing stats
 print(f"Antall stolpeturer: {stolpeturer['count']}")
 print("Total stolpejaktlengde: {} km".format(m_to_km(stolpeturer["total_distance"])))
 print("Total antall høydemeter: {} m (Nærmeste fjelltopp nådd: {} - {}m)".format(stolpeturer["total_elevation_gain"],

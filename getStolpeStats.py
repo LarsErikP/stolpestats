1396c6dc937d3b2d246b4d32efccbe8d2bc7b5da
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-06-28 23:38:32 +0200

    Lagt til lengste tid brukt. Også litt refactor

--- getStolpeStats.py
+++ getStolpeStats.py
@@ -1,6 +1,6 @@
 import json
 import csv
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from stravalib import Client
 
@@ -11,16 +11,26 @@ def m_to_km(dist):
 def mps_to_kmt(speed):
     return round(speed*3.6,1)
 
+def print_duration(seconds):
+    h, m, s = str(timedelta(seconds=seconds)).split(':')
+    return "{} timer, {} minutter og {} sekunder".format(h,m,s)
+
 def closest_mountain(target_height):
     name, (num, height) = min(fjell.items(), key=lambda item: abs(item[1][1] - target_height))
     return (name, num, height)
+
+def update_stat(stat_name, name, date, data_field, data):
+    stolpeturer[stat_name]["name"] = name
+    stolpeturer[stat_name]["date"] = date
+    stolpeturer[stat_name][data_field] = data
 
 # Global variables
 stolpeturer = { "count": 0, 
                "total_distance": 0.0,
                "total_elevation_gain": 0.0,
                "longest_trip": { "distance": 0, "name": "", "date": "" },
-               "fastest_trip": { "avg_speed": 0.0, "name": "", "date": "" }
+               "fastest_trip": { "avg_speed": 0.0, "name": "", "date": "" },
+               "longest_time": { "seconds" : 0, "name": "", "date": "" }
                }
 
 after = "2025-01-01"
@@ -51,18 +61,20 @@ client = Client(
 activities = client.get_activities(after=after)
 for activity in activities:
     if "stolpe" in activity.name.casefold():
+        name = activity.name
+        date = activity.start_date.strftime("%d.%m.%Y")
         stolpeturer["count"] += 1
         stolpeturer["total_distance"] += activity.distance
         stolpeturer["total_elevation_gain"] += activity.total_elevation_gain
+
         if activity.distance > stolpeturer["longest_trip"]["distance"]:
-            stolpeturer["longest_trip"]["distance"] = activity.distance
-            stolpeturer["longest_trip"]["name"] = activity.name
-            stolpeturer["longest_trip"]["date"] = activity.start_date.strftime("%d.%m.%Y")
+            update_stat("longest_trip", name, date, "distance", activity.distance)
 
         if activity.average_speed > stolpeturer["fastest_trip"]["avg_speed"]:
-            stolpeturer["fastest_trip"]["avg_speed"] = activity.average_speed
-            stolpeturer["fastest_trip"]["name"] = activity.name
-            stolpeturer["fastest_trip"]["date"] = activity.start_date.strftime("%d.%m.%Y")
+            update_stat("fastest_trip", name, date, "avg_speed", activity.average_speed)
+
+        if activity.elapsed_time > stolpeturer["longest_time"]["seconds"]:
+            update_stat("longest_time", name, date, "seconds", activity.elapsed_time)
 
 mountain = closest_mountain(stolpeturer["total_elevation_gain"])
 
@@ -80,8 +92,14 @@ print("Lengste tur: {} - {} ({} km)".format(stolpeturer["longest_trip"]["date"],
                                             m_to_km(stolpeturer["longest_trip"]["distance"])
                                             )
      )
+print("Lengst tid: {} - {} ({})".format(stolpeturer["longest_time"]["date"],
+                                        stolpeturer["longest_time"]["name"],
+                                        print_duration(stolpeturer["longest_time"]["seconds"])
+                                       )
+     )
 print("Raskeste tur: {} - {} (Snittfart {} km/t)".format(stolpeturer["fastest_trip"]["date"],
                                                          stolpeturer["fastest_trip"]["name"],
                                                          mps_to_kmt(stolpeturer["fastest_trip"]["avg_speed"])
                                                         )
      )
+
